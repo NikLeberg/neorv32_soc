5783b3142d5c5270636438bae264919d11564690
NikLeberg <niklaus.leuenb+github@gmail.com>
2021-12-27 03:19:21 +0100

    keypad_reader: wip implementation

--- vhdl/keypad_reader.vhdl
+++ vhdl/keypad_reader.vhdl
@@ -3,7 +3,7 @@
 --
 -- Authors:                 Niklaus Leuenberger <leuen4@bfh.ch>
 --
--- Version:                 0.1
+-- Version:                 0.2
 --
 -- Entity:                  keypad_reader
 --
@@ -13,6 +13,8 @@
 --
 -- Changes:                 0.1, 2021-12-10, leuen4
 --                              initial version
+--                          0.2, 2021-12-27, leuen4
+--                              work in progress implementation
 -- =============================================================================
 
 LIBRARY ieee;
@@ -20,17 +22,107 @@ USE ieee.std_logic_1164.ALL;
 
 ENTITY keypad_reader IS
     PORT (
-        clock  : IN STD_LOGIC;
-        reset  : IN STD_LOGIC;
-        column : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-        row    : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+        clock : IN STD_LOGIC;
+        reset : IN STD_LOGIC;
+        rows  : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
 
-        pressed : OUT STD_LOGIC; -- 1 if a key was pressed (active for 1 clock)
+        columns : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
         -- hexadecimal value of pressed key, 0 = 0x0, 1 = 0x1, ..., F = 0xF
         key : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
     );
 END ENTITY keypad_reader;
 
 ARCHITECTURE no_target_specific OF keypad_reader IS
+    TYPE state_type IS (
+        COLUMN_1,
+        COLUMN_2,
+        COLUMN_3,
+        COLUMN_4
+    );
+    SIGNAL s_current_state : state_type := COLUMN_1;
+    SIGNAL s_next_state : state_type := COLUMN_1;
+    SIGNAL s_rows : STD_LOGIC_VECTOR(3 DOWNTO 0); -- synchronized copy of rows input
 BEGIN
+    -- =========================================================================
+    -- Purpose: Synchronize inputs to clock cycle, e.g. save in register
+    -- Type:    sequential
+    -- Inputs:  clock, rows
+    -- Outputs: s_rows
+    -- =========================================================================
+    sync_inputs : PROCESS (clock) IS
+    BEGIN
+        IF (rising_edge(clock)) THEN
+            s_rows <= rows;
+        END IF;
+    END PROCESS sync_inputs;
+
+    -- =========================================================================
+    -- Purpose: State memory with synchronous reset
+    -- Type:    sequential
+    -- Inputs:  clock, reset, s_next_state
+    -- Outputs: s_current_state
+    -- =========================================================================
+    state_memory : PROCESS (clock) IS
+    BEGIN
+        IF (rising_edge(clock)) THEN
+            IF (reset = '1') THEN
+                s_current_state <= COLUMN_1;
+            ELSE
+                s_current_state <= s_next_state;
+            END IF;
+        END IF;
+    END PROCESS state_memory;
+
+    -- =========================================================================
+    -- Purpose: Next state logic for FSM
+    -- Type:    combinational
+    -- Inputs:  s_current_state, row
+    -- Outputs: s_next_state
+    -- =========================================================================
+    nsl : PROCESS (s_current_state) IS
+    BEGIN
+        -- cyclic activation of each column
+        CASE (s_current_state) IS
+            WHEN COLUMN_1 =>
+                s_next_state <= COLUMN_2;
+            WHEN COLUMN_2 =>
+                s_next_state <= COLUMN_3;
+            WHEN COLUMN_3 =>
+                s_next_state <= COLUMN_4;
+            WHEN COLUMN_4 =>
+                s_next_state <= COLUMN_1;
+            WHEN OTHERS =>
+                s_next_state <= COLUMN_1;
+        END CASE;
+    END PROCESS nsl;
+
+    -- =========================================================================
+    -- Purpose: Output logic for FSM
+    -- Type:    combinational
+    -- Inputs:  s_current_state, s_rows
+    -- Outputs: columns, key
+    -- =========================================================================
+    columns <= "1110" WHEN s_current_state = COLUMN_1 ELSE
+        "1101" WHEN s_current_state = COLUMN_2 ELSE
+        "1011" WHEN s_current_state = COLUMN_3 ELSE
+        "0111" WHEN s_current_state = COLUMN_4 ELSE
+        "1111";
+    -- ToDo: Maybe column needs to be one state shifted
+    key <= x"1" WHEN s_current_state = COLUMN_1 AND s_rows = "1110" ELSE
+        x"4" WHEN s_current_state = COLUMN_1 AND s_rows = "1101" ELSE
+        x"7" WHEN s_current_state = COLUMN_1 AND s_rows = "1011" ELSE
+        x"0" WHEN s_current_state = COLUMN_1 AND s_rows = "0111" ELSE
+        x"2" WHEN s_current_state = COLUMN_2 AND s_rows = "1110" ELSE
+        x"5" WHEN s_current_state = COLUMN_2 AND s_rows = "1101" ELSE
+        x"8" WHEN s_current_state = COLUMN_2 AND s_rows = "1011" ELSE
+        x"F" WHEN s_current_state = COLUMN_2 AND s_rows = "0111" ELSE
+        x"3" WHEN s_current_state = COLUMN_3 AND s_rows = "1110" ELSE
+        x"6" WHEN s_current_state = COLUMN_3 AND s_rows = "1101" ELSE
+        x"9" WHEN s_current_state = COLUMN_3 AND s_rows = "1011" ELSE
+        x"E" WHEN s_current_state = COLUMN_3 AND s_rows = "0111" ELSE
+        x"A" WHEN s_current_state = COLUMN_4 AND s_rows = "1110" ELSE
+        x"B" WHEN s_current_state = COLUMN_4 AND s_rows = "1101" ELSE
+        x"C" WHEN s_current_state = COLUMN_4 AND s_rows = "1011" ELSE
+        x"D" WHEN s_current_state = COLUMN_4 AND s_rows = "0111" ELSE
+        x"0";
 END ARCHITECTURE no_target_specific;
---
+++ vhdl/keypad_reader_tb.vhdl
@@ -0,0 +1,96 @@
+-- =============================================================================
+-- File:                    keypad_reader_tb.vhdl
+--
+-- Authors:                 Niklaus Leuenberger <leuen4@bfh.ch>
+--
+-- Version:                 0.1
+--
+-- Entity:                  keypad_reader_tb
+--
+-- Description:             Read in the Pmod Keyboard from Digilent over the 16
+--                          pin interface of row and column lines. For more,
+--                          see: https://digilent.com/reference/pmod/pmodkypd
+--
+-- Changes:                 0.1, 2021-12-27, leuen4
+--                              initial version
+-- =============================================================================
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE std.env.stop;
+
+ENTITY keypad_reader_tb IS
+    -- testbench needs no ports
+END ENTITY keypad_reader_tb;
+
+ARCHITECTURE simulation OF keypad_reader_tb IS
+    -- component definition for device under test
+    COMPONENT keypad_reader
+        PORT (
+            clock : IN STD_LOGIC;
+            reset : IN STD_LOGIC;
+            rows  : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+
+            columns : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+            -- hexadecimal value of pressed key, 0 = 0x0, 1 = 0x1, ..., F = 0xF
+            key : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
+        );
+    END COMPONENT keypad_reader;
+    -- signals for connecting to the DUT
+    SIGNAL s_clock : STD_LOGIC := '0';
+    SIGNAL s_reset : STD_LOGIC := '1';
+    SIGNAL s_rows : STD_LOGIC_VECTOR(3 DOWNTO 0) := "1111";
+    SIGNAL s_columns : STD_LOGIC_VECTOR(3 DOWNTO 0);
+    SIGNAL s_key : STD_LOGIC_VECTOR(3 DOWNTO 0);
+BEGIN
+    -- instantiate the device under test
+    dut : keypad_reader
+    PORT MAP(
+        clock   => s_clock,
+        reset   => s_reset,
+        rows    => s_rows,
+        columns => s_columns,
+        key     => s_key
+    );
+
+    -- clock with 100 MHz
+    s_clock <= NOT s_clock AFTER 5 ns;
+
+    -- power on reset the DUT
+    s_reset <= '1', '0' AFTER 20 ns;
+
+    test : PROCESS IS
+    BEGIN
+        -- wait for power on reset to finish
+        WAIT UNTIL falling_edge(s_reset);
+
+        -- test the cyclic activation of columns
+        ASSERT s_columns = "1110"
+        REPORT "Column 1 is not active." SEVERITY failure;
+        WAIT ON s_columns;
+        ASSERT s_columns = "1101"
+        REPORT "Column 2 is not active." SEVERITY failure;
+        WAIT ON s_columns;
+        ASSERT s_columns = "1011"
+        REPORT "Column 3 is not active." SEVERITY failure;
+        WAIT ON s_columns;
+        ASSERT s_columns = "0111"
+        REPORT "Column 4 is not active." SEVERITY failure;
+
+        -- test the decoding of "1" key
+        s_rows <= "1110";
+        WAIT UNTIL s_columns = "1110";
+        ASSERT s_key = x"1"
+        REPORT "Expected key 1 but got " & to_hstring(s_key) & "." SEVERITY failure;
+
+        -- test the decoding of "5" key
+        s_rows <= "1101";
+        WAIT UNTIL s_columns = "1101";
+        ASSERT s_key = x"5"
+        REPORT "Expected key 5 but got " & to_hstring(s_key) & "." SEVERITY failure;
+
+        -- report successful test
+        REPORT "Test OK";
+        stop;
+    END PROCESS test;
+END ARCHITECTURE simulation;
