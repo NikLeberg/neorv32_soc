9fcd8aa47b424d51bb1df7090950a21d6c2802ff
NikLeberg <niklaus.leuenb+github@gmail.com>
2022-01-02 19:20:30 +0100

    quartus: silence warning "no clock uncertainty"

--- scripts/io_assignment/clocks_sdc.tcl
+++ scripts/io_assignment/clocks_sdc.tcl
@@ -4,3 +4,4 @@ set_time_unit ns
 set_decimal_places 3
 create_clock -period 83.333 -waveform { 0 41.667 } clock -name clk1
 # create_clock -period 20.0 -waveform { 0 10.0 } <ENTITY_PORT_NAME_CONNECTED_TO_CLOCK_50MHZ> -name clk2
+derive_clock_uncertainty -add
