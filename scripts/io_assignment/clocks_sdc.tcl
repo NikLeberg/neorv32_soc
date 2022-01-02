8212c3ca103bcdb1361128517c5565fb10d80e5b
NikLeberg <niklaus.leuenb+github@gmail.com>
2022-01-02 19:26:50 +0100

    quartus: use correct 50 MHz clock definition

--- scripts/io_assignment/clocks_sdc.tcl
+++ scripts/io_assignment/clocks_sdc.tcl
@@ -2,6 +2,6 @@
 # of your top entity
 set_time_unit ns
 set_decimal_places 3
-create_clock -period 83.333 -waveform { 0 41.667 } clock -name clk1
-# create_clock -period 20.0 -waveform { 0 10.0 } <ENTITY_PORT_NAME_CONNECTED_TO_CLOCK_50MHZ> -name clk2
+# create_clock -period 83.333 -waveform { 0 41.667 } <ENTITY_PORT_NAME_CONNECTED_TO_CLOCK_12MHZ> -name clk1
+create_clock -period 20.0 -waveform { 0 10.0 } clock -name clk2
 derive_clock_uncertainty -add
