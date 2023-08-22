94a7652edd234b55d2bf43c09f1ec4ae016a1130
NikLeberg <niklaus.leuenb+github@gmail.com>
2023-08-22 14:10:35 +0000

    misc: update scripts

--- scripts/sim_open.tcl
+++ scripts/sim_open.tcl
@@ -10,12 +10,12 @@ add wave *
 add wave -divider dut
 catch {add wave dut/*}
 
+# only show short names of signals
+config wave -signalnamewidth 1
+
 # run simulation until stop, break or wait
 run -all
 
 # go to wave panel and zoom to the full extend
 view wave
 wave zoom full
-
-# only show short names of signals
-config wave -signalnamewidth 1
