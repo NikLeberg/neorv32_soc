571195c01d99fca6f63723af4383744633681c10
NikLeberg <niklaus.leuenb+github@gmail.com>
2023-09-30 21:19:11 +0200

    rtl/neorv32_wb_gateway: reduce latency

--- vhdl/neorv32_wb_io/neorv32_wb_gateway.vhdl
+++ vhdl/neorv32_wb_io/neorv32_wb_gateway.vhdl
@@ -3,7 +3,7 @@
 --
 -- Authors:                 Niklaus Leuenberger <leuen4@bfh.ch>
 --
--- Version:                 0.2
+-- Version:                 0.3
 --
 -- Entity:                  neorv32_wb_gateway
 --
@@ -15,6 +15,8 @@
 --                              initial version
 --                          0.2, 2023-08-05, leuen4
 --                              allow bus to be locked by lr/sc atomic ops
+--                          0.3, 2023-09-30, leuen4
+--                              reduce latency, immediately forward request
 -- =============================================================================
 
 LIBRARY ieee;
@@ -45,9 +47,10 @@ END ENTITY neorv32_wb_gateway;
 ARCHITECTURE no_target_specific OF neorv32_wb_gateway IS
 
     -- Register for pending bus access (rden_i or wren_i was asserted).
-    SIGNAL pending_request : STD_ULOGIC;
+    SIGNAL pending_read : STD_ULOGIC; -- set on read request
     SIGNAL pending_write : STD_ULOGIC; -- set on write request
     SIGNAL pending_atomic : STD_ULOGIC; -- lr/sc pair in progress
+    SIGNAL active_request : STD_ULOGIC; -- a request is currently active
 
 BEGIN
 
@@ -58,17 +61,18 @@ BEGIN
     BEGIN
         IF rising_edge(clk_i) THEN
             IF rstn_i = '0' THEN
-                pending_request <= '0';
+                pending_read <= '0';
                 pending_write <= '0';
                 pending_atomic <= '0';
             ELSE
                 -- Start a request on either re or we assertion. Request is
                 -- pending until either ack or err was returned.
-                IF pending_request = '0' AND (req_i.re OR req_i.we) = '1' THEN
-                    pending_request <= '1';
-                    pending_write <= req_i.we;
-                ELSIF pending_request = '1' AND (wb_master_i.ack OR wb_master_i.err) = '1' THEN
-                    pending_request <= '0';
+                IF req_i.re = '1' THEN
+                    pending_read <= '1';
+                ELSIF req_i.we = '1' THEN
+                    pending_write <= '1';
+                ELSIF (wb_master_i.ack OR wb_master_i.err) = '1' THEN
+                    pending_read <= '0';
                     pending_write <= '0';
                 END IF;
                 -- Mark request as atomic read/write pair if rvso bit is set.
@@ -82,9 +86,13 @@ BEGIN
         END IF;
     END PROCESS proc_request;
 
+    -- A request of active if the current cycle has a request, or a previous
+    -- request was not fullfulled yet.
+    active_request <= req_i.re OR req_i.we OR pending_read OR pending_write;
+
     -- Map CPU bus to Wishbone.
-    wb_master_o.cyc <= pending_request OR pending_atomic; -- hold on to slave if atomic access
-    wb_master_o.stb <= pending_request;
+    wb_master_o.cyc <= active_request OR pending_atomic; -- hold on to slave if atomic access
+    wb_master_o.stb <= active_request;
     wb_master_o.we <= pending_write;
     wb_master_o.sel <= req_i.ben;
     wb_master_o.adr <= req_i.addr;
